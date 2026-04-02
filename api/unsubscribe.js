9223c74b171ebdc5ae1a65a217ceec9b05161b07
nikkosantopietro <nikkosantopietro@gmail.com>
2026-04-02 10:15:18 -0700

    Update unsubscribe.js

--- api/unsubscribe.js
+++ api/unsubscribe.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 const sgMail = require('@sendgrid/mail');
-
+// v2
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_SERVICE_KEY
